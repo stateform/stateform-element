4c2697708810017a5e96ea5d9f3ccd6065e10f32
LiangWeiliang <liangweiliang@coocaa.com>
2019-01-08 15:27:48 +0800

    feat:rename InputNumber to Counter

---
+++ src/lib/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <InputNumber
+    :value="value"
+    @input="$event !== value && $emit('input', $event)"
+    :placeholder="placeholder"
+  >
+  </InputNumber>
+</template>
+
+<script>
+import FormItem from './FormItem.js'
+export default {
+  mixins: [FormItem]
+}
+</script>
